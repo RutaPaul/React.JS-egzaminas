416df8b403487eb6886697cff4c754015cce4f8d
RutaPaul <rutapaulaviciute14@gmail.com>
2023-03-29 22:22:56 +0300

    implemented moving navigation bar

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -3,149 +3,111 @@ import NavigationLink from "./NavigationLink";
 import "./navbar.css";
 
 const Navbar = (props) => {
-    const [visiblePositions, setVisiblePositions] = useState([]);
-
+    const [visiblePositions, setVisiblePositions] = useState([0,1,2,3,4,5,6,7,8,9]);
+    
     const availableLinks =  [
         {
             positon: 0,
-            key: "",
-            icon: "sss"
+            key: "Lakefront",
+            icon: "fa-solid fa-house-tsunami"
         },
         {
             positon: 1,
-            key: "Lakefront",
-            icon: "sss"
+            key: "Amazing views",
+            icon: "fa-solid fa-mountain-sun"
         },
         {
             positon: 2,
-            key: "Amazing views",
-            icon: "sss"
+            key: "Trending",
+            icon: "fa-solid fa-fire"
         },
         {
             positon: 3,
-            key: "",
-            icon: "sss"
+            key: "Domes",
+            icon: "fa-solid fa-house-chimney"
+        },
+        {
+            positon: 4,
+            key: "Islands",
+            icon: "fa-solid fa-campground"
+        },
+        {
+            positon: 5,
+            key: "Mansions",
+            icon: "fa-solid fa-chess-rook"
+        },
+        {
+            positon: 6,
+            key: "Beachfront",
+            icon: "fa-solid fa-umbrella-beach"
+        },
+        {
+            positon: 7,
+            key: "Treehouses",
+            icon: "fa-solid fa-tree-city"
+        },
+        {
+            positon: 8,
+            key: "Luxe",
+            icon: "fa-brands fa-confluence"
+        },
+        {
+            positon: 9,
+            key: "Castles",
+            icon: "fa-brands fa-fort-awesome"
+        },
+        {
+            positon: 10,
+            key: "OMG!",
+            icon: "fa-brands fa-space-awesome"
+        },
+        {
+            positon: 11,
+            key: "Creating spaces",
+            icon: "fa-solid fa-palette"
+        },
+        {
+            positon: 12,
+            key: "Tropical",
+            icon: "fa-solid fa-tree"
+        },
+        {
+            positon: 13,
+            key: "Tiny homes",
+            icon: "fa-solid fa-igloo"
+        },
+        {
+            positon: 14,
+            key: "Design",
+            icon: "fa-solid fa-paint-roller"
+        },
+        {
+            positon: 15,
+            key: "Grand pianos",
+            icon: "fa-solid fa-icons"
         }
     ]
 
-
-
-
+    const allPositions = availableLinks.map(el=>el.positon);
+    
     return (
-
-
-
-
         <div className="navbar">
+
+            <NavigationLink text="LEFT" icon="fa-solid fa-circle-arrow-left" divClass="navLink" setType={props.setType} isHidden={false} 
+                setVisiblePositions={setVisiblePositions} visiblePositions={visiblePositions} allPositions={allPositions}
+            />
             {
                 availableLinks.map(el => (
                     props.type === el.key ?
-                    <NavigationLink text={el.key} icon={el.icon} divClass="navLinkActive" setType={props.setType}  isHidden={visiblePositions.includes(el.positon)}/>
+                    <NavigationLink text={el.key} icon={el.icon} divClass="navLinkActive" setType={props.setType}  isHidden={!visiblePositions.includes(el.positon)}/>
                     : 
-                    <NavigationLink text={el.key} icon={el.icon} divClass="navLink" setType={props.setType}  isHidden={visiblePositions.includes(el.positon)}/>
+                    <NavigationLink text={el.key} icon={el.icon} divClass="navLink" setType={props.setType}  isHidden={!visiblePositions.includes(el.positon)}/>
                 ))
             }
+            <NavigationLink text="RIGHT" icon="fa-solid fa-circle-arrow-right" divClass="navLink" setType={props.setType} isHidden={false} 
+                setVisiblePositions={setVisiblePositions} visiblePositions={visiblePositions} allPositions={allPositions}
+            />
 
-
-            <NavigationLink text="" icon="fa-solid fa-circle-arrow-left" divClass="navLink" setType={props.setType} isHidden={false}/>
-            {
-                props.type === "Lakefront" ?
-                <NavigationLink text="Lakefront" icon="fa-solid fa-house-tsunami" divClass="navLinkActive" setType={props.setType}  isHidden={true}/>
-                : 
-                <NavigationLink text="Lakefront" icon="fa-solid fa-house-tsunami" divClass="navLink" setType={props.setType}  isHidden={true}/>
-            }
-            {
-                props.type === "Amazing views" ?
-                <NavigationLink text="Amazing views" icon="fa-solid fa-mountain-sun" divClass="navLinkActive" setType={props.setType}  isHidden={true}/>
-                : 
-                <NavigationLink text="Amazing views" icon="fa-solid fa-mountain-sun" divClass="navLink" setType={props.setType}  isHidden={true}/>
-            }
-            {
-                props.type === "Trending" ?
-                <NavigationLink text="Trending" icon="fa-solid fa-fire" divClass="navLinkActive" setType={props.setType}  isHidden={true}/>
-                :
-                <NavigationLink text="Trending" icon="fa-solid fa-fire" divClass="navLink" setType={props.setType}  isHidden={true}/>
-            }
-            {
-                props.type === "Domes" ?
-                <NavigationLink text="Domes" icon="fa-solid fa-house-chimney" divClass="navLinkActive" setType={props.setType}  isHidden={true}/>
-                :
-                <NavigationLink text="Domes" icon="fa-solid fa-house-chimney" divClass="navLink" setType={props.setType}  isHidden={true}/>
-            }
-            {
-                props.type === "Islands" ?
-                <NavigationLink text="Islands" icon="fa-solid fa-campground" divClass="navLinkActive" setType={props.setType}  isHidden={true}/>
-                :
-                <NavigationLink text="Islands" icon="fa-solid fa-campground" divClass="navLink" setType={props.setType}  isHidden={true}/>
-            }
-            {
-                props.type === "Mansions" ?
-                <NavigationLink text="Mansions" icon="fa-solid fa-chess-rook" divClass="navLinkActive" setType={props.setType}  isHidden={true}/>
-                :
-                <NavigationLink text="Mansions" icon="fa-solid fa-chess-rook" divClass="navLink" setType={props.setType}  isHidden={true}/>
-            }
-            {
-                props.type === "Beachfront" ?
-                <NavigationLink text="Beachfront" icon="fa-solid fa-umbrella-beach" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Beachfront" icon="fa-solid fa-umbrella-beach" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Treehouses" ?
-                <NavigationLink text="Treehouses" icon="fa-solid fa-tree-city" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Treehouses" icon="fa-solid fa-tree-city" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Luxe" ?
-                <NavigationLink text="Luxe" icon="fa-brands fa-confluence" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Luxe" icon="fa-brands fa-confluence" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Castles" ?
-                <NavigationLink text="Castles" icon="fa-brands fa-fort-awesome" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Castles" icon="fa-brands fa-fort-awesome" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "OMG!" ?
-                <NavigationLink text="OMG!" icon="fa-brands fa-space-awesome" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="OMG!" icon="fa-brands fa-space-awesome" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Creating spaces" ?
-                <NavigationLink text="Creating spaces" icon="fa-solid fa-palette" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Creating spaces" icon="fa-solid fa-palette" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Tropical" ?
-                <NavigationLink text="Tropical" icon="fa-solid fa-tree" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Tropical" icon="fa-solid fa-tree" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Tiny homes" ?
-                <NavigationLink text="Tiny homes" icon="fa-solid fa-igloo" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Tiny homes" icon="fa-solid fa-igloo" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Design" ?
-                <NavigationLink text="Design" icon="fa-solid fa-paint-roller" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Design" icon="fa-solid fa-paint-roller" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-            {
-                props.type === "Grand pianos" ?
-                <NavigationLink text="Grand pianos" icon="fa-solid fa-icons" divClass="navLinkActive" setType={props.setType}  isHidden={false}/>
-                :
-                <NavigationLink text="Grand pianos" icon="fa-solid fa-icons" divClass="navLink" setType={props.setType}  isHidden={false}/>
-            }
-
-            <NavigationLink text="" icon="fa-solid fa-circle-arrow-right" divClass="navLink" setType={props.setType}  isHidden={false}/>
         </div>
     )
 }
--- my-app/src/components/Navbar/NavigationLink/NavigationLink.jsx
+++ my-app/src/components/Navbar/NavigationLink/NavigationLink.jsx
@@ -1,22 +1,61 @@
 import React, {useState, useEffect} from "react";
 import "./navigationlink.css";
 
-const NavigationLink = ({text, icon, divClass, setType, isHidden}) => {
+const NavigationLink = ({text, icon, divClass, setType, isHidden, setVisiblePositions, visiblePositions, allPositions}) => {
 
     return (
         isHidden ? "" :
         <div className={divClass} onClick={()=>{
-            setType(text);
+            switch(text){
+                case "LEFT" :
+                    if(visiblePositions[0] == allPositions[0]) return;
+                    visiblePositions.pop();
+                    visiblePositions.unshift(visiblePositions[0]-1);
+                    setVisiblePositions(FixVisibleArray(allPositions,visiblePositions));
+                    break;
+                case "RIGHT" :
+                    if(visiblePositions[visiblePositions.length-1] == allPositions[allPositions.length-1]) return;
+                    visiblePositions.shift();
+                    visiblePositions.push(visiblePositions[visiblePositions.length-1]+1);
+                    setVisiblePositions(FixVisibleArray(allPositions,visiblePositions));
+                    break;
+                default :
+                    setType(text)
+                    break;
+            }
             }} >
              <div className="navLinkIcon">
                 <i className={icon} />
             </div>
             <div className="navLinkText">
-                {text}
+                {text == "LEFT" ? "" : text == "RIGHT" ? "" : text }
             </div>
         </div>
         
     )
+}
+
+function FixVisibleArray (allPositions, visiblePositions) {
+    const newVisiblePositions = [];
+        
+    visiblePositions.forEach(pos=>{
+        if(!allPositions.includes(pos)){
+            if(pos < 0){
+                pos = allPositions[allPositions.length-1];
+            }
+
+            if(pos >= allPositions.length){
+                pos = allPositions[0];
+            }
+
+            newVisiblePositions.push(pos);
+        }
+        else {
+            newVisiblePositions.push(pos);
+        }
+    })
+
+    return newVisiblePositions;
 }
 
 export default NavigationLink;
